90f6d0ffbdb157a997e7b11edae8ab2831acb0ec
gaoyapei1993 <44640045+gaoyapei1993@users.noreply.github.com>
2019-02-12 12:17:39 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  mounted(){
+    //当根组件挂载完成后需要异步请求全局配置数据
+    var url = this.$store.state.globalSettings.apiUrl+'/admin/settings';
+    
+    this.$axios.get(url).then((res)=>{
+      this.$store.commit('setGlobalSettings',res.data);  //将全局配置存储到Vuex存储仓库
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+}  
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #303133;
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
 </style>
